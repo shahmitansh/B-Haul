e39f12d452752386c9110041aaceecd1f1e6c942
Sanketh Hegde <san2heg@gmail.com>
2019-11-12 12:26:40 -0800

    Create and style sidebar items with mock data

--- frontend/src/MapView/MapView.js
+++ frontend/src/MapView/MapView.js
@@ -7,12 +7,25 @@ import HeaderBuy from '../Header/HeaderBuy';
 
 class MapView extends React.Component {
   render() {
+    const sampleItem = {
+      title: "Small Wooden Dining Table",
+      description: "A small wooden dining table.",
+      properties: {
+        color: "Brown",
+        distance: "0.2 mi",
+        size: "2.1 ft x 6.3 ft x 4.2 ft",
+        elevation: "112 ft UP 52 ft DOWN"
+      }
+    };
+
     return (
       <div id="container">
         <HeaderBuy />
         <div id="map-container">
           <div id="sidebar">
-            <Sidebar />
+            <Sidebar
+              items={[sampleItem]}
+            />
           </div>
           <div id="map">
             <Map />
--- frontend/src/Sidebar/Sidebar.js
+++ frontend/src/Sidebar/Sidebar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
 
+import SidebarItem from './SidebarItem/SidebarItem';
+
 class Sidebar extends React.Component {
   render() {
+    const sidebarItemList = this.props.items.map((item) =>
+      <SidebarItem
+        item={item}
+      />
+    );
+
     return (
-      <div>Sidebar</div>
+      <div className="sidebar-container">
+        {sidebarItemList}
+      </div>
     );
   }
 }
