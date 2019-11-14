cb955ebb0649466cf69a27bf147f36cfbbc12d91
Manish Rao <manurao@g.ucla.edu>
2019-11-14 03:15:10 -0800

    Update listing items

--- frontend/src/MapView/MapView.js
+++ frontend/src/MapView/MapView.js
@@ -7,7 +7,7 @@ import HeaderBuy from '../Header/HeaderBuy';
 
 class MapView extends React.Component {
   render() {
-    const sampleItem = {
+    const sampleItem1 = {
       title: "Small Wooden Dining Table",
       description: "A small wooden dining table.",
       location: {
@@ -16,13 +16,88 @@ class MapView extends React.Component {
       },
       properties: {
         color: "Brown",
-        distance: "0.2 mi",
+        distance: "0.3 mi",
         size: "2.1 ft x 6.3 ft x 4.2 ft",
         elevation: "112 ft UP 52 ft DOWN"
       }
     };
 
-    const items = [sampleItem, sampleItem, sampleItem, sampleItem, sampleItem, sampleItem];
+    const sampleItem2 = {
+      title: "Big Wooden Dining Table",
+      description: "A small wooden dining table.",
+      location: {
+        lat: 34.067370,
+        lng: -118.452740
+      },
+      properties: {
+        color: "Black",
+        distance: "0.2 mi",
+        size: "8.1 ft x 3.3 ft x 5.2 ft",
+        elevation: "12 ft UP 5 ft DOWN"
+      }
+    };
+
+    const sampleItem3 = {
+      title: "Cute Coffee Table",
+      description: "A small wooden dining table.",
+      location: {
+        lat: 34.066280,
+        lng: -118.450370
+      },
+      properties: {
+        color: "Blue",
+        distance: "0.5 mi",
+        size: "3.1 ft x 4.3 ft x 3.2 ft",
+        elevation: "133 ft UP 25 ft DOWN"
+      }
+    };
+
+    const sampleItem4 = {
+      title: "TV Table",
+      description: "A small wooden dining table.",
+      location: {
+        lat: 34.068740,
+        lng: -118.447560
+      },
+      properties: {
+        color: "White",
+        distance: "0.4 mi",
+        size: "2.1 ft x 6.3 ft x 4.2 ft",
+        elevation: "112 ft UP 52 ft DOWN"
+      }
+    };
+
+    const sampleItem5 = {
+      title: "Small Wooden Dining Table",
+      description: "A small wooden dining table.",
+      location: {
+        lat: 34.064150,
+        lng: -118.452730
+      },
+      properties: {
+        color: "Coffee",
+        distance: "0.3 mi",
+        size: "2.1 ft x 6.3 ft x 4.2 ft",
+        elevation: "112 ft UP 52 ft DOWN"
+      }
+    };
+
+    const sampleItem6 = {
+      title: "Small Wooden Dining Table",
+      description: "A small wooden dining table.",
+      location: {
+        lat: 34.070040,
+        lng: -118.453400
+      },
+      properties: {
+        color: "Mocha",
+        distance: "0.1 mi",
+        size: "2.1 ft x 6.3 ft x 4.2 ft",
+        elevation: "112 ft UP 52 ft DOWN"
+      }
+    };
+
+    const items = [sampleItem1, sampleItem2, sampleItem3, sampleItem4, sampleItem5, sampleItem6];
 
     return (
       <div id="container">
