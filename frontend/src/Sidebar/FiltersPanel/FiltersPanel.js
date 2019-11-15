ee1301bbb139cb27d1e7c1cf35285928b6cc3332
Sanketh Hegde <san2heg@gmail.com>
2019-11-14 19:45:52 -0800

    Add filter dropdown to sidebar

---
+++ frontend/src/Sidebar/FiltersPanel/FiltersPanel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FiltersPanel.css';
+
+class FiltersPanel extends React.Component {
+  render() {
+    return (
+      <div id="filters">
+
+      </div>
+    );
+  }
+}
+
+export default FiltersPanel;
--- frontend/src/Sidebar/Sidebar.js
+++ frontend/src/Sidebar/Sidebar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Sidebar.css';
+import downArrow from './sort-down.svg';
 
 import SidebarItem from './SidebarItem/SidebarItem';
+import FiltersPanel from './FiltersPanel/FiltersPanel';
 
 class Sidebar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {filtersVisible: false};
+  }
+
+  handleClick() {
+    this.setState(state => ({filtersVisible: !state.filtersVisible}));
+  }
+
   render() {
     const sidebarItemList = this.props.items.map((item, index) =>
       <SidebarItem
@@ -14,7 +27,11 @@ class Sidebar extends React.Component {
 
     return (
       <div className="sidebar-container">
-        {sidebarItemList}
+        <span id="show-filter-header" onClick={this.handleClick}>
+          <img style={{transform: this.state.filtersVisible ? 'rotate(180deg)' : ''}} className="sort-arrow" height="10px" src={downArrow} />
+          Show filters
+        </span>
+        {this.state.filtersVisible ? <FiltersPanel /> : sidebarItemList}
       </div>
     );
   }
