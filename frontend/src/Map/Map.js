b8bc6bfcc4134ea7f1c7f123e57eef2af38e9bde
Manish Rao <manurao@g.ucla.edu>
2019-11-14 19:39:20 -0800

    Compute distance to user

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import axios from 'axios';
 
 import ListingPin from './ListingPin';
 import UserPin from './UserPin';
@@ -12,7 +13,8 @@ class Map extends React.Component {
       latitude: 34.0699,
       longitude: -118.4462,
       zoom: 14.3
-    }
+    },
+    // distance: 0
   };
 
   onViewportChange = viewport => {
@@ -36,13 +38,25 @@ class Map extends React.Component {
     );
   }
 
+  // computeDistance =(userLats, userLngs, itemLat, itemLng) => {
+  //   const url = 'https://www.mapquestapi.com/directions/v2/routematrix?key=aGF9qhMVGLXeMA5UGCdSZt7rIIp600r8&json={locations:[%20{%20latLng:{%20lat:' + userLats + ',%20lng:' + userLngs +'%20}%20},%20{%20latLng:{%20lat:' + itemLat + ',%20lng:'+ itemLng + '}%20}%20],%20options:{%20manyToOne:true%20}}'
+  //   console.log(url)
+  //   axios.get(url)
+  //     .then(response => 
+  //       {
+  //         console.log(response, 'resp')
+  //         this.setState({distance: response.data.distance[1]})
+  //         console.log(this.state.distance, 'dist')
+  //       })
+  // }
+
   render () {
     const {
       viewport,
     } = this.state
 
     const [userLat, userLng] = [this.props.userLat, this.props.userLng];
-
+    
     return (
       <ReactMapGL
         width='100%'
@@ -52,11 +66,15 @@ class Map extends React.Component {
         onViewportChange={viewport => this.onViewportChange(viewport)}
         mapStyle="mapbox://styles/mapbox/streets-v11"
       >
+        {/* <div> <button onClick={this.computeDistance(userLat, userLng, lat[0], lng[0])}> Click Me! </button></div> */}
+        {/* <a> {this.state.distance} </a> */}
         {this.props.items.map(this._renderListingMarker)}
         {(userLat && userLng) && this._renderUserMarker(userLat, userLng)}
       </ReactMapGL>
     )
   }
 }
-
+// http://www.mapquestapi.com/directions/v2/routematrix?key=aGF9qhMVGLXeMA5UGCdSZt7rIIp600r8
+// http://www.mapquestapi.com/directions/v2/routematrix?key=aGF9qhMVGLXeMA5UGCdSZt7rIIp600r8&json=locations:[%20{%20latLng:{%20lat:40.3612,%20lng:-76.794%20}%20},%20{%20latLng:{%20lat:40.3021,%20lng:-76.9463}%20}%20],%20options:{%20allToAll:true%20}}
+// http://www.mapquestapi.com/directions/v2/routematrix?key=aGF9qhMVGLXeMA5UGCdSZt7rIIp600r8&json={locations:[%20{%20latLng:{%20lat:40.3612,%20lng:-76.794%20}%20},%20{%20latLng:{%20lat:40.3021,%20lng:-76.9463}%20}%20],%20options:{%20manyToOne:true%20}}
 export default Map;
--- frontend/src/MapView/MapView.js
+++ frontend/src/MapView/MapView.js
@@ -1,103 +1,146 @@
 import React from 'react';
 import './MapView.css';
 
+import axios from 'axios';
 import Map from '../Map/Map';
 import Sidebar from '../Sidebar/Sidebar';
 import HeaderBuy from '../Header/HeaderBuy';
 
 class MapView extends React.Component {
+  state = {
+    distance: 0
+  }
+
+  userLat = this.props.userLat
+  userLng = this.props.userLng
+
+  sampleItem1 = {
+    title: "Small Wooden Dining Table",
+    description: "A small wooden dining table.",
+    location: {
+      lat: 34.067138,
+      lng: -118.451128
+    },
+    properties: {
+      color: "Brown",
+      distance: "",
+      size: "2.1 ft x 6.3 ft x 4.2 ft",
+      elevation: "112 ft UP 52 ft DOWN"
+    }
+  };
+
+  sampleItem2 = {
+    title: "Big Wooden Dining Table",
+    description: "A small wooden dining table.",
+    location: {
+      lat: 34.067370,
+      lng: -118.452740
+    },
+    properties: {
+      color: "Black",
+      distance: "",
+      size: "8.1 ft x 3.3 ft x 5.2 ft",
+      elevation: "12 ft UP 5 ft DOWN"
+    }
+  };
+
+  sampleItem3 = {
+    title: "Cute Coffee Table",
+    description: "A small wooden dining table.",
+    location: {
+      lat: 34.066280,
+      lng: -118.450370
+    },
+    properties: {
+      color: "Blue",
+      distance: "",
+      size: "3.1 ft x 4.3 ft x 3.2 ft",
+      elevation: "133 ft UP 25 ft DOWN"
+    }
+  };
+
+  sampleItem4 = {
+    title: "TV Table",
+    description: "A small wooden dining table.",
+    location: {
+      lat: 34.068740,
+      lng: -118.447560
+    },
+    properties: {
+      color: "White",
+      distance: "",
+      size: "2.1 ft x 6.3 ft x 4.2 ft",
+      elevation: "112 ft UP 52 ft DOWN"
+    }
+  };
+
+  sampleItem5 = {
+    title: "Small Wooden Dining Table",
+    description: "A small wooden dining table.",
+    location: {
+      lat: 34.064150,
+      lng: -118.452730
+    },
+    properties: {
+      color: "Coffee",
+      distance: "",
+      size: "2.1 ft x 6.3 ft x 4.2 ft",
+      elevation: "112 ft UP 52 ft DOWN"
+    }
+  };
+
+  sampleItem6 = {
+    title: "Small Wooden Dining Table",
+    description: "A small wooden dining table.",
+    location: {
+      lat: 34.070040,
+      lng: -118.453400
+    },
+    properties: {
+      color: "Mocha",
+      distance: "",
+      size: "2.1 ft x 6.3 ft x 4.2 ft",
+      elevation: "112 ft UP 52 ft DOWN"
+    }
+  };
+
+  items = [this.sampleItem1, this.sampleItem2, this.sampleItem3, this.sampleItem4, this.sampleItem5, this.sampleItem6];
+
+  lat = [];
+  lng = [];
+
+  computeDistance =() => {
+    setTimeout(() => {
+      for(var i = 0; i < this.items.length; i++) {
+        this.compute_helper(i)
+      }
+    }, 5000)
+  }
+  
+  compute_helper =(loop) => {
+    const userLats = this.props.userLat
+    const userLngs = this.props.userLng
+    this.lat.push(this.items[loop].location.lat)
+    this.lng.push(this.items[loop].location.lng)
+    var itemLat=this.lat[loop]
+    var itemLng=this.lng[loop]
+    const url = 'https://www.mapquestapi.com/directions/v2/routematrix?key=aGF9qhMVGLXeMA5UGCdSZt7rIIp600r8&json={locations:[%20{%20latLng:{%20lat:' + userLats + ',%20lng:' + userLngs +'%20}%20},%20{%20latLng:{%20lat:' + itemLat + ',%20lng:'+ itemLng + '}%20}%20],%20options:{%20manyToOne:true%20}}'
+
+    axios.get(url)
+      .then(response => 
+        {
+          this.items[loop].properties.distance = response.data.distance[1].toString() + " mi"
+          this.setState({distance: this.items[loop].properties.distance})
+        }
+      )
+  }
+
+  componentDidMount(){
+    this.computeDistance()
+  }
+
   render() {
-    const sampleItem1 = {
-      title: "Small Wooden Dining Table",
-      description: "A small wooden dining table.",
-      location: {
-        lat: 34.067138,
-        lng: -118.451128
-      },
-      properties: {
-        color: "Brown",
-        distance: "0.3 mi",
-        size: "2.1 ft x 6.3 ft x 4.2 ft",
-        elevation: "112 ft UP 52 ft DOWN"
-      }
-    };
-
-    const sampleItem2 = {
-      title: "Big Wooden Dining Table",
-      description: "A small wooden dining table.",
-      location: {
-        lat: 34.067370,
-        lng: -118.452740
-      },
-      properties: {
-        color: "Black",
-        distance: "0.2 mi",
-        size: "8.1 ft x 3.3 ft x 5.2 ft",
-        elevation: "12 ft UP 5 ft DOWN"
-      }
-    };
-
-    const sampleItem3 = {
-      title: "Cute Coffee Table",
-      description: "A small wooden dining table.",
-      location: {
-        lat: 34.066280,
-        lng: -118.450370
-      },
-      properties: {
-        color: "Blue",
-        distance: "0.5 mi",
-        size: "3.1 ft x 4.3 ft x 3.2 ft",
-        elevation: "133 ft UP 25 ft DOWN"
-      }
-    };
-
-    const sampleItem4 = {
-      title: "TV Table",
-      description: "A small wooden dining table.",
-      location: {
-        lat: 34.068740,
-        lng: -118.447560
-      },
-      properties: {
-        color: "White",
-        distance: "0.4 mi",
-        size: "2.1 ft x 6.3 ft x 4.2 ft",
-        elevation: "112 ft UP 52 ft DOWN"
-      }
-    };
-
-    const sampleItem5 = {
-      title: "Small Wooden Dining Table",
-      description: "A small wooden dining table.",
-      location: {
-        lat: 34.064150,
-        lng: -118.452730
-      },
-      properties: {
-        color: "Coffee",
-        distance: "0.3 mi",
-        size: "2.1 ft x 6.3 ft x 4.2 ft",
-        elevation: "112 ft UP 52 ft DOWN"
-      }
-    };
-
-    const sampleItem6 = {
-      title: "Small Wooden Dining Table",
-      description: "A small wooden dining table.",
-      location: {
-        lat: 34.070040,
-        lng: -118.453400
-      },
-      properties: {
-        color: "Mocha",
-        distance: "0.1 mi",
-        size: "2.1 ft x 6.3 ft x 4.2 ft",
-        elevation: "112 ft UP 52 ft DOWN"
-      }
-    };
-
-    const items = [sampleItem1, sampleItem2, sampleItem3, sampleItem4, sampleItem5, sampleItem6];
+    const items = this.items;
 
     return (
       <div id="container">
