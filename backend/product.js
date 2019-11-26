d7115bb7d628960989c7e9d264cd37fde3c3cac4
Ken Gu <ken.qgu@gmail.com>
2019-11-25 20:43:41 -0800

    add more filter endpoints and include more paramters

--- backend/product.js
+++ backend/product.js
@@ -1,8 +1,10 @@
 const productTypes = {"table": 1, "bed": 2, "chair": 3, "couch": 4};
+const productColors = {"red": 1, "green": 2, "blue":3, "black":4, "grey":5, "brown":6, "orange":7, "white":8}
 const filterFactory =  require('./filterFactory.js');
 
 class Product {
-	constructor(productID, title, elevation, address, description, sellerID, price, type, location){
+	constructor(productID, title, elevation, address, description, 
+		sellerID, price, type, location, hasElevator, color, size){
 		this.productID = productID;
 		this.title = title;
 		this.address = address;
@@ -10,11 +12,20 @@ class Product {
 		this.sellerID = sellerID;
 		this.price = price;
 		this.elevation = elevation;
+		this.hasElevator = hasElevator
+		this.size = size
+		if (!(color in productColors)){
+			console.log(`product color ${color} not in allowed product colors ${JSON.stringify(productColors)}`);
+		}
 		// console.log( "couch" in productTypes)
-		if (!(type in productTypes)){
-			console.log(`product type ${type} not in allowed product types ${JSON.stringify(productTypes)}`);
+		let arraylength = type.length;
+		for (let i = 0; i < arraylength; i++){
+			if (!(type[i] in productTypes)){
+				console.log(`product type ${type[i]} not in allowed product types ${JSON.stringify(productTypes)}`);
+			}
 		}
 		this.type = type;
+		this.color = color
 		this.location = location;
 	}
 }
@@ -49,7 +60,14 @@ class ProductList{
 		let upperBoundedProducts = filterFactory.filterMethod(this.products, upper_bound, "pricelower");
 		return filterFactory.filterMethod(upperBoundedProducts, lower_bound, "pricehigher");
 	}
-		
+
+	returnFilteredProductsColor(color){
+		return filterFactory.filterMethod(this.products, color, "color");
+	}
+
+	returnFilteredProductsSize(size){
+		return filterFactory.filterMethod(this.products, size, "size");
+	}
 
 }
 
