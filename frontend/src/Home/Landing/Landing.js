9374c4ede01891a0ea39d541f57086158b929834
Sanketh Hegde <san2heg@gmail.com>
2019-11-26 20:01:11 -0800

    Pass filters as URL parameters

--- frontend/src/Home/Landing/Landing.js
+++ frontend/src/Home/Landing/Landing.js
@@ -6,21 +6,35 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
 const options = [
-  'Desk', 'Table', 'Bed', 'Chair'
+  'All', 'Desk', 'Table', 'Bed', 'Chair'
 ]
 const defaultOption = options[0];
 
 export default class Landing extends Component {
+
+    constructor(props) {
+      super(props);
+      this._onSelect = this._onSelect.bind(this);
+      this._onClick = this._onClick.bind(this);
+      this.value = defaultOption;
+    }
+
+    _onSelect(target) {
+      this.value = target.value;
+    }
+
+    _onClick() {
+      window.location = `/preference?type=${this.value.toLowerCase()}`;
+    }
+
     render(){
         return (
             <div className="middle-home-container">
                 <div className="Landing-Prompt"> What are you looking for? </div>
-                <Dropdown className='Landing-Options' options={options} value={defaultOption} />
+                <Dropdown onChange={this._onSelect} className='Landing-Options' options={options} value={defaultOption}/>
                 <div className="Landing-Click">
-                    <a  href="/preference"
+                    <a  onClick={this._onClick}
                         className="Landing-Color"
-                        target="_self"
-                        rel="noopener noreferrer"
                         > Next > </a>
                 </div>
             </div>
