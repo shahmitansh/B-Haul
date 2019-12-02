667b5bedaa00448ce1cebf6a41685b257b5fa620
brianlee22 <56566703+brianlee22@users.noreply.github.com>
2019-12-02 03:15:25 -0800

    Update product.js jsdocs

--- backend/product.js
+++ backend/product.js
@@ -3,6 +3,16 @@ const productColors = {"red": 1, "green": 2, "blue":3, "black":4, "grey":5, "bro
 const filterFactory =  require('./filterFactory.js');
 
 class Product {
+	
+	/**
+ 	* Represents Product object.
+ 	* @constructor
+ 	* @param {Number} productID - ID to represent unique Product
+ 	* @param {String} name - Name of Product
+	* @param {Number} elevation - Elevation information for where the product is being sold from
+	* @param {String} address - Address of where the Seller plans to meet with potential Buyers
+	* @param {String} description - Description of Product
+ 	*/
 	constructor(productID, name, elevation, address, description, 
 		sellerID, price, type, location, hasElevator, color, size){
 		this.productID = productID;
@@ -31,6 +41,11 @@ class Product {
 }
 
 class ProductList{
+	/**
+ 	* Represents a list of all the products stored in the database.
+ 	* @constructor
+ 	* @param {ProductList} productList - ProductList object that is used to initialize the original list of products
+ 	*/
 	constructor(productList){
 		if(productList == undefined){
 			this.products = {};
@@ -39,10 +54,18 @@ class ProductList{
 		}
 	}
 
+	/**
+ 	* Adds a product to the database.
+	* @param {Product} product - Product object that is to be added to the database
+ 	*/
 	addProduct(product){
 		this.products[product.productID] = product;
 	}
 
+	/**
+ 	* Removes a product from the database.
+	* @param {Number} productID - Unique ID of prod that is to be removed from the database
+ 	*/
 	removeProduct(productID){
 		if (!(productID in this.products)){
 			console.log(`product with productID ${productID} not in product list`);
@@ -51,20 +74,41 @@ class ProductList{
 		}
 	}
 
+	/**
+ 	* Returns a list of Products filtered by type.
+	* @param {string} type - Describes the type of product e.g. desk, chair, couch, etc.
+	* @return {Product[]} - An array of Products filtered by type
+ 	*/
 	returnFilteredProductsType(type){
 		console.log("inside returnFilteredProductsType")
 		return filterFactory.filterMethod(this.products, type, "type");
 	}
 
+	/**
+ 	* Returns a list of Products filtered by type.
+	* @param {Number} lower_bound - Lower bound price range
+	* @param {Number} upper_bound - Upper bound price range
+	* @return {Product[]} - An array of Products filtered by price either by price lower than or price higher than
+ 	*/
 	returnFilteredProductsPrice(lower_bound, upper_bound){
 		let upperBoundedProducts = filterFactory.filterMethod(this.products, upper_bound, "pricelower");
 		return filterFactory.filterMethod(upperBoundedProducts, lower_bound, "pricehigher");
 	}
 
+	/**
+ 	* Returns a list of Products filtered by color.
+	* @param {string} type - Describes the color of the product
+	* @return {Product[]} - An array of Products filtered by color
+ 	*/
 	returnFilteredProductsColor(color){
 		return filterFactory.filterMethod(this.products, color, "color");
 	}
 
+	/**
+ 	* Returns a list of Products filtered by size.
+	* @param {string} type - Describes the size of the product
+	* @return {Product[]} - An array of Products filtered by size
+ 	*/
 	returnFilteredProductsSize(size){
 		return filterFactory.filterMethod(this.products, size, "size");
 	}
