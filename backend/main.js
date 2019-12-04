f5a3a1ffec32b1d7676b738b8a1316b328fa26da
Sanketh Hegde <san2heg@gmail.com>
2019-12-03 22:04:40 -0800

    Add backend endpoint for adding AWS s3 images

--- backend/main.js
+++ backend/main.js
@@ -11,6 +11,17 @@ const port = 3000;
 const FilterFactory = require('./filterFactory.js')
 const MongoDao = require('./dao.js');
 
+const aws = require('aws-sdk');
+require('dotenv').config(); // Configure dotenv to load in the .env file
+// Configure aws with your accessKeyId and your secretAccessKey
+aws.config.update({
+  region: 'us-west-1', // Put your aws region here
+  accessKeyId: process.env.AWSAccessKeyId,
+  secretAccessKey: process.env.AWSSecretKey
+})
+
+const S3_BUCKET = process.env.Bucket
+
 //Serving static files from the react app as necessary
 //app.use(express.static(path.join(__dirname, '../frontend/src')))
 
@@ -18,6 +29,34 @@ const MongoDao = require('./dao.js');
 var mongoDao = null;
 const url = 'mongodb+srv://admin:supereasytormb@cluster0-bgrbj.mongodb.net/test?retryWrites=true&w=majority';
 const dbName = 'bhaul';
+
+app.post('/sign_s3', (req,res) => {
+  const s3 = new aws.S3();  // Create a new instance of S3
+  const fileName = req.body.fileName;
+  const fileType = req.body.fileType;
+// Set up the payload of what we are sending to the S3 api
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 500,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+// Make a request to the S3 API to get a signed URL which we can use to upload our file
+s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      res.json({success: false, error: err})
+    }
+    // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved.
+const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    // Send it all back
+    res.json({success:true, data:{returnData}});
+  });
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -144,6 +183,7 @@ app.post('/addListing', async function(request, response, next){
 		let productID = await nextProductID()
 		let doc = request.body
 		let product = new Product(productID, doc["name"], doc["elevation"], doc["address"], doc["description"], doc["sellerID"], doc["price"], doc["type"], doc["location"], doc["hasElevator"], doc["color"], doc["size"], doc["imageURL"])
+		console.log(product);
 		await mongoDao.insertDocument("products", product, () => {});
 		response.send("Successfully inserted document")
 
